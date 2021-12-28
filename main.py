3d755a144f07778466f69a87f71805b9dc25ce38
Emil Krzaczyński <emil.krzakuou@gmail.com>
2021-12-28 21:23:51 +0100

    refactor main function by adding exception and add flask to requirements.txt

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-import requests
-import functions_framework
-import os
 import json
+import os
+import time
+import traceback
 
+import functions_framework
+import requests
+from flask import abort
 
 headers = {"Authorization": f"token {os.getenv('PERSONAL_TOKEN')}"}
 
 
 @functions_framework.http
-def run_fetch(request):
-    data = fetch_api_v4("facebook", "graphql")
-    return data
+def main(request):
+    try:
+        if "owner" not in request.args or "repo" not in request.args:
+            return abort(400, "'owner' and 'repo' are required'")
+        owner = request.args.get("owner")
+        repo_name = request.args.get("repo")
+        data_v4 = fetch_api_v4(owner, repo_name)
+        print(data_v4)
+    except Exception as e:
+        print(f"Trace: {traceback.format_exc()}")
+        time.sleep(5)
+        raise e
+    return ""
 
 
 def fetch_api_v4(owner: str, repo_name: str):
@@ -41,13 +54,13 @@ def fetch_api_v4(owner: str, repo_name: str):
         )
 
 
-# def fetch_api_v3(owner: str, repo_name: str, path: str):
-#     url = f"https://api.github.com/repos/{owner}/{repo_name}/{path}"
-#     response = requests.get(url, headers=headers)
-#     data = response.json()
-#     if response.status_code == 200:
-#         return data
-#     else:
-#         raise Exception(
-#             f"Query failet to run by returning code of {response.status_code, query}"
-#         )
+def fetch_api_v3(owner: str, repo_name: str, path: str):
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/{path}"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    if 200 < response.status_code < 300:
+        return data
+    else:
+        raise Exception(
+            f"Query failet to run by returning code of {response.status_code, query}"
+        )
